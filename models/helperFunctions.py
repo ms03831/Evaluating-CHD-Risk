294f545de9b97855346a853f19c111a3318ae97c
Mudasir <ms03831@st.habib.edu.pk>
2019-11-27 06:49:41 +0500

    neural network somewhat working

--- models/helperFunctions.py
+++ models/helperFunctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import random
 
 #sigmoid(z)
 def sigmoid(z): 
@@ -6,6 +7,7 @@ def sigmoid(z):
 
 #MSE
 def cost(yPred, y): 
+    yPred, y = np.array(yPred), np.array(y)
     return 0.5*(yPred-y)**2
 
 #derivative of cost 
@@ -14,16 +16,23 @@ def derivateCost(yPred, y):
 
 #iterator
 def dataIter(batchSize, data):
-    random.shuffle(data)
+    data = np.array(data)
+    #random.shuffle(data)
     batches = []
-    for i in range(0, data.shape[0], batchSize):
-        batches.append(data[i:i+batchSize])
+
+    for i in range(0, np.array(data[0]).shape[0], batchSize):
+        batchX, batchY = np.array(data[0][i:i+batchSize]), np.array(data[1][i:i+batchSize])
+        batches.append([batchX, batchY])
     return batches
 
 #accuracy
-def accuracy(net, actual, predicted):
-    accuracy = np.sum(actual == predicted).mean() * 100
-    return accuracy
+def accuracy(predicted, actual):
+    predicted = predicted > 0.5
+    actual = np.array(actual).reshape(actual.shape[0])
+    predicted = np.array(predicted).reshape(predicted.shape[0])
+    
+    acc = (np.sum(actual == predicted)/len(actual)) * 100
+    return acc
 
 #normalize, scale btw 0 and 1
 def normalize(x):
@@ -34,10 +43,9 @@ def normalize(x):
 #calculates error of entire batch.
 def calculateError(net, dataset):
     j = 0
-    for data in dataset:
-        yPred = net.forwardPass(data[0])
-        y = data[1]
-        costEx = cost(yPred, y) #error of single example.
-        j += sum(costEx)
-    return j/len(trainData) #taking mean
+    yPred = net.forwardPass(dataset[0])
+    y = dataset[1]
+    costEx = cost(yPred, y) #error of single example.
+    j = costEx.sum()
+    return j/len(dataset[0]) #taking mean
     
